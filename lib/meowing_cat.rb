7bf1fc5b25e1325fef124e56df88e73a33e39c44
Vinn Nduri <ndurivo@gmail.com>
2022-08-25 09:56:33 +0300

    commit tree ece3f4f14dad5be6060b881937cb148d6253397c

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+# # code your solution here. 
+require 'pry'
+class Cat 
+
+    attr_accessor :name
+    
+    def meow
+        puts "meow!"
+    end
+end
+
+cat = Cat.new
+cat.name = "Maru"
+puts cat.name
+cat.meow
+
+
+
+
